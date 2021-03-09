8bcb3bfd54400ea8c865531f503dceb852fb1ba1
cassjacques <75631685+cassjacques@users.noreply.github.com>
2021-03-09 09:05:15 -0800

    built file uploader

---
+++ src/components/Upload/FileUploadApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FileUpload from './FileUpload';
+import './App.css';
+
+const Uploader = () => (
+    <div className='container mt-4'>
+        <h4 className='display-4 text-center mb-4'>
+        </h4>
+    </div>
+);
+
+export default Uploader;
