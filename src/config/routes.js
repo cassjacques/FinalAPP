6ec5023a58b818614f4782c278233e52b2f4eed8
Cassandra <75631685+cassjacques@users.noreply.github.com>
2023-08-14 17:36:48 -0700

    Update routes.js

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,4 +1,4 @@
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Routes, Route, Redirect } from 'react-router-dom';
 
 import HomePage from '../pages/HomePage';
 import AboutPage from '../pages/AboutPage';
@@ -16,23 +16,23 @@ import ProfilePage from '../pages/User/ProfilePage';
 import EditProfilePage from '../pages/User/EditProfilePage';
 
 const routes = ({ token, setToken }) => (
-  <Switch>
-    <Route exact path='/' component={HomePage} />
-    <Route exact path='/about' component={AboutPage} />
-    <Route path='/signup' component={SignupPage} />
-    <Route path='/login' component={() => <LoginPage setToken={setToken} />} />
-    <Route path='/edit-profile/:id' component={EditProfilePage} />
-    {token ? <Route path='/profile' component={ProfilePage} /> : <Redirect to='login' />} 
-    <Route path='/fits' component={FitsPage} />
-    <Route path='/new-fit' component={NewFitPage} />
-    <Route path='/edit-fit/:id' component={EditFitPage} />
-    <Route path='/show-fit/:id' component={ShowFitPage} />
-    <Route path='/soss' component={SOSsPage} />
-    <Route path='/new-sos' component={NewSOSPage} />
-    <Route path='/edit-sos/:id' component={EditSOSPage} />
-    <Route path='/show-sos/:id' component={ShowSOSPage} />
-    <Route path='*' component={() => <div><h2>Page Not Found</h2></div>} />
-  </Switch>
+  <Routes>
+    <Route exact path='/' element={<HomePage/>} />
+    <Route exact path='/about' element={<AboutPage/>} />
+    <Route path='/signup' element={<SignupPage/>} />
+    <Route path='/login' element={<() => <LoginPage setToken={<setToken/>} />/>} />
+    <Route path='/edit-profile/:id' element={<EditProfilePage/>} />
+    {<token ? <Route path='/profile' element={<ProfilePage/>} /> : <Redirect to='login' />/>} 
+    <Route path='/fits' element={<FitsPage/>} />
+    <Route path='/new-fit' element={<NewFitPage/>} />
+    <Route path='/edit-fit/:id' element={<EditFitPage/>} />
+    <Route path='/show-fit/:id' element={<ShowFitPage/>} />
+    <Route path='/soss' element={<SOSsPage/>} />
+    <Route path='/new-sos' element={<NewSOSPage/>} />
+    <Route path='/edit-sos/:id' element={<EditSOSPage/>} />
+    <Route path='/show-sos/:id' element={<ShowSOSPage/>} />
+    <Route path='*' element={() => <div><h2>Page Not Found</h2></div>} />
+  </Routes>
 );
 
 export default routes;
